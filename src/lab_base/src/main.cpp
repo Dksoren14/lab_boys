03ab3087c993ae73a2af731091eb24a312c01925
Søren <dksoren14@gmail.com>
2026-02-25 13:15:56 +0100

    updated msg

--- src/lab_base/src/main.cpp
+++ src/lab_base/src/main.cpp
@@ -3,34 +3,82 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <ctime>
 
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <rcutils/logging.h>
 
+#include "interfaces/msg/socket_msg.hpp"
 
+using namespace std;
 
-
-
-
-int main()
+class LabBaseNode : public rclcpp::Node
 {
-    // creating socket
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+public:
+    LabBaseNode() 
+    : Node("lab_base_node")
+    {
+    
+         rclcpp::QoS qos(10);
+         qos.reliability(rclcpp::ReliabilityPolicy::BestEffort);
+         qos.durability(rclcpp::DurabilityPolicy::TransientLocal);  
+         std::cout << "Starting client.... " << std::endl;
 
-    // specifying address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080); //converting to network byte order
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    // sending connection request
-    connect(clientSocket, (struct sockaddr*)&serverAddress,
-            sizeof(serverAddress));
 
-    // sending data
-    const char* message = "Hello, server!";
-    send(clientSocket, message, strlen(message), 0);
+         test_publisher = create_publisher<interfaces::msg::SocketMsg>(
+            "/lab/lab_chassis/in/socket_msg", qos
+         );
+        
+            // Start a thread to send messages
+        test_timer = create_wall_timer(1s, std::bind(&LabBaseNode::sendMessage, this)); // Send a message every 200 milliseconds
+    }
 
-    // closing socket
-    close(clientSocket);
 
+private:
+    rclcpp::Publisher<interfaces::msg::SocketMsg>::SharedPtr test_publisher;
+    rclcpp::TimerBase::SharedPtr test_timer;
+    time_t current_time;
+
+    void sendMessage()
+    {
+        
+        interfaces::msg::SocketMsg message;
+
+        message.timestamp = this->get_clock()->now().seconds();
+        message.data = "Hello, manipulator!";
+        test_publisher->publish(message);
+
+        
+  
+    }
+};
+
+int main(int argc, char * argv[])
+{
+    //// creating socket
+    //int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+//
+    //// specifying address
+    //sockaddr_in serverAddress;
+    //serverAddress.sin_family = AF_INET;
+    //serverAddress.sin_port = htons(8080); //converting to network byte order
+    //serverAddress.sin_addr.s_addr = INADDR_ANY;
+//
+    //// sending connection request
+    //connect(clientSocket, (struct sockaddr*)&serverAddress,
+    //        sizeof(serverAddress));
+//
+    //// sending data
+    //const char* message = "Hello, server!";
+    //send(clientSocket, message, strlen(message), 0);
+//
+    //// closing socket
+    //close(clientSocket);
+
+    rclcpp::init(argc, argv);;
+    rclcpp::spin(std::make_shared<LabBaseNode>());
+    rclcpp::shutdown();
     return 0;
 }
--- src/lab_manipulator/src/main.cpp
+++ src/lab_manipulator/src/main.cpp
@@ -4,44 +4,81 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <rcutils/logging.h>
+
+#include "interfaces/msg/socket_msg.hpp"
+
 using namespace std;
 
 
 
+class LabManipulatorNode : public rclcpp::Node
+{
+public:
+    LabManipulatorNode() 
+    : Node("lab_manipulator_node")
+    {
+        rclcpp::QoS qos(10);
+        qos.reliability(rclcpp::ReliabilityPolicy::BestEffort);
+        qos.durability(rclcpp::DurabilityPolicy::TransientLocal);
+
+        std::cout << "Starting server.... " << std::endl;
 
 
+        sub_state = create_subscription<interfaces::msg::SocketMsg>(
+            "/lab/lab_chassis/out/socket_msg", qos,
+            [this](const interfaces::msg::SocketMsg::SharedPtr msg)
+            { messageCallback(msg); }
+            );
+
+      
 
 
+    }
+private:
 
+    void messageCallback(const interfaces::msg::SocketMsg::SharedPtr msg)
+    {
+        timestamp = msg->timestamp;
+        data = msg->data;
+    }
 
-int main() {
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    rclcpp::Subscription<interfaces::msg::SocketMsg>::SharedPtr sub_state;
 
-    // specifying the address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    float timestamp;
+    string data;
 
-    // binding socket.
-    bind(serverSocket, (struct sockaddr*)&serverAddress,
-         sizeof(serverAddress));
+    void receiveMessage()
+    {
+        // creating socket
+        int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // listening to the assigned socket
-    listen(serverSocket, 5);
+        // specifying address
+        sockaddr_in serverAddress;
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(8080); //converting to network byte order
+        serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    // accepting connection request
-    int clientSocket
-        = accept(serverSocket, nullptr, nullptr);
+        // sending connection request
+        connect(clientSocket, (struct sockaddr*)&serverAddress,
+                sizeof(serverAddress));
 
-    // recieving data
-    char buffer[1024] = { 0 };
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout << "Message from client: " << buffer
-              << endl;
+        // sending data
+        const char* message = "Hello, server!";
+        send(clientSocket, message, strlen(message), 0);
 
-    // closing the socket.
-    close(serverSocket);
+        // closing socket
+        close(clientSocket);
+    }
+    
 
+};
+int main(int argc, char * argv[]) {
+    
+    rclcpp::init(argc, argv);;
+    rclcpp::spin(std::make_shared<LabManipulatorNode>());  
+    rclcpp::shutdown();
     return 0;
 }
