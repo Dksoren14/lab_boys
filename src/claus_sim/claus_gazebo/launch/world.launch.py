e5a696e4a807c4d12fdc90c42f197db58a6712eb
Snekkie <buschkasper@gmail.com>
2026-03-27 14:25:23 +0100

    Made a table in gazebo

--- src/claus_sim/claus_gazebo/launch/world.launch.py
+++ src/claus_sim/claus_gazebo/launch/world.launch.py
@@ -1,14 +1,18 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-
     pkg_path = get_package_share_directory('claus_gazebo')
     world = os.path.join(pkg_path, 'worlds', 'sim_environment.world')
+    models = os.path.join(pkg_path, 'models')
 
     return LaunchDescription([
+        SetEnvironmentVariable(
+            name='GZ_SIM_RESOURCE_PATH',
+            value=models
+        ),
         ExecuteProcess(
             cmd=['gz', 'sim', world],
             output='screen'
