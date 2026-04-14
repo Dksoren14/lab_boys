0d6df5e682fa74cf0c2d44d4e42c4c3320afe09e
Snekkie <buschkasper@gmail.com>
2026-04-14 15:47:22 +0200

    Add: A new cpp file for Odom_to_TF and some extra plugins for world file. Fixed: Lidar code was wrong and missing /scan output

--- install/setup.sh
+++ install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/mathias/lab_boys/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/snekkie/lab_boys/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
