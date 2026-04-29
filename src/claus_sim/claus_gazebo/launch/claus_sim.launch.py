609a26f85c989a3572856526620c200f03bb7cde
Mathias Ask Hansen <mathiasaskhansen04@gmail.com>
2026-04-29 12:53:41 +0200

    Add: Added Intel Realsense D435i to the robot. Rviz2 config needs to be changed to include the image output of the camera

--- src/claus_sim/claus_gazebo/launch/claus_sim.launch.py
+++ src/claus_sim/claus_gazebo/launch/claus_sim.launch.py
@@ -227,6 +227,9 @@ def generate_launch_description():
             '/sensors/front_lidar/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
             '/model/r100/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
             '/world/default/model/r100/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model',
+            '/front_realsense/image@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/front_realsense/depth_image@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/front_realsense/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked'
         ],
         remappings=[
             ('/world/default/clock', '/clock'),
